1b385c1b95470f103c008b1cbfc41a7c93439710
AndreyYarohovich <yarohovich@gmail.com>
2019-10-17 16:39:47 +0300

    A little refactor

--- test/test.go
+++ test/test.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/a-yarohovich/mlog"
 )
 
 func main() {
-	mlog.InitLog("test", mlog.OutputAll)
+	file, err := os.OpenFile("test.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	multi := io.MultiWriter(file, os.Stdout)
+	mlog.SetFlags(mlog.Ldate | mlog.Lmicroseconds | mlog.Lshortfile | mlog.Lcolor)
+	mlog.SetOutput(multi)
+
 	mlog.Error("This is an error")
 	mlog.Debug("Debug")
 	mlog.Info("Info")
 	mlog.Warning("Warning")
-	mlog.Fatal("Fatal")
+	//mlog.Fatal("Fatal")
 }
